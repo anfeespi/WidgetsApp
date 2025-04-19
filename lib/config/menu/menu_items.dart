7bbd88bece8ef07058d3d793a3da9ba741ac8880
anfeespi <andres.espitia.afer@gmail.com>
2025-04-19 01:00:49 -0500

    Infinite scroll finalizado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -57,5 +57,11 @@ const appMenuItems = <MenuItem>[
       subtitle: "Pequeño tutorial introductorio",
       link: "/tutorial",
       icon: Icons.accessible_rounded
+  ),
+  MenuItem(
+      title: "Infinite scroll y pull",
+      subtitle: "Listas infinitas y pull to refresh",
+      link: "/infinite",
+      icon: Icons.list_alt_rounded
   )
 ];
